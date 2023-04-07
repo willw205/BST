85c89fe36a5da5bd3333328944ed96fd07b0d5d7
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-04-07 10:34:33 -0700

    Added in the user commands and layout of where functions will go

--- main.cpp
+++ main.cpp
@@ -22,8 +22,62 @@
    cout << "--------------------------------------" << endl;
 
    cout << "This is BST! Burgers, Milk Shakes, and Tequila! Hold up... I meant Binary Search Tree!" << endl; 
-   
- }
 
+ do {
 
+   cout << "Hey User! Input the number 1 if you want to create the numbers through a file or input the number 2 to create them manually";
+
+   cin >> userIn;
+   cin.get();
+
+   if (answer == 1) {
+     //Input the file function here
+     continue = true;
+   } else if (answer == 2) {
+     //Input the user file function here
+     continue = true;
+   } else {
+     cout << endl;
+     "Invalid input! Repeat again" << endl;
+   }
+ } while (!cont);
+
+ cout << "--------------------------------------" << endl;
+ print(root, 0);
+
+  int numz = 0;
+
+ do {
+   cout << "--------------------------------------" << endl;
+   cout << "Please choose the follow commands --> ADD or SEARCH or REMOVE or PRINT or QUIT: ";
+   cin.get(input, 50);
+   cin.get();
+
+   if (strcmp(input, "ADD") == 0) {
+     cout << "Please enter what number you would to add: ";
+     cin >> numz;
+     cin.get();
+     //Insertion function here
+     cout << "Successfully Added!" << endl;
+   } else if (strcmp(input, "SEARCH") == 0) {
+     cout << "Please enter the number you want to look for: ";
+     cin >> numz;
+     cin.get();
+     //Search function here
+   } else if (strcmp(input, "REMOVE") == 0) {
+     cout << "Please enter the number you want to remove: ";
+     cin >> numz;
+     cin.get();
+     //remove function here
+     cout << "Successfull Removed!" << endl;
+   } else if (strcmp(input, "PRINT") == 0) {
+     //print function here
+   } else if (strcmp(input, "QUIT")) {
+     cout << "Thanks for using this application!" << endl;
+   } else {
+     cout << "Invalid command. Please try again!" << endl;
+   }
+ } while (strcmpy(input, "QUIT") != 0);
+
+}
 
