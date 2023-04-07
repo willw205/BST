6c045c9c90a3203c24d2584e2e8c73ad3fceeef9
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-04-07 10:02:39 -0700

    Constructor w/paramters & Destructor

---
+++ node.h
@@ -0,0 +1,14 @@
+ #include <iostream>
+ using namespace std;
+
+ class Node {
+  public:
+   
+   int data;
+   Node * left;
+   Node * right;
+   
+   Node(int data, Node * right, Node * left);
+   Node();
+   ~Node();
+ };
