d5434fd6effe5e2488c9a23e55a7a4d453848bef
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-04-07 10:14:28 -0700

    Welcoming user, setting up file

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+ //William Wong
+ //4/7/23
+ //This project allows one to create, insert, search, delete, and traverse
+ //a binary tree data structure!
+
+ #include <cstring>
+ #include <iosstream>
+ #include <fstream>
+ #include <vector>
+ #include "node.h"
+ using namespace std;
+
+ node * root = NULL;
+
+ int main() {
+
+   int userIn = 0;
+   char input[50];
+   bool continue = false;
+
+   cout << "--------------------------------------" << endl;
+
+   cout << "This is BST! Burgers, Milk Shakes, and Tequila! Hold up... I meant Binary Search Tree!" << endl; 
+   
+ }
+
+
+
